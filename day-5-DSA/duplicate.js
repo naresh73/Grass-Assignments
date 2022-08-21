204c785a9eed396ffdbb90724e652e4e52a11936
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-08-21 18:06:28 +0530

    remove duplicate element from array

---
+++ day-5-DSA/duplicate.js
@@ -0,0 +1,14 @@
+const id = [10,23,45,67,10,67,455,231];
+
+//this fuction will remove repeat element from array
+function removeDuplicate(arr) {
+    const result = [];
+    for (let i=0 ; i<arr.length ; i++) {
+        if (!result.includes(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    console.log(result);
+}
+
+removeDuplicate(id);
