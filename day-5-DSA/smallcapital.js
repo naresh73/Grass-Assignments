a0f64608ad0e3b00c6bc4db2901bb0aaaa371a40
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-08-21 18:30:11 +0530

    capital & small latter

---
+++ day-5-DSA/smallcapital.js
@@ -0,0 +1,19 @@
+let latter = ['A','y','Z','a','B','b','r','u'];
+
+//this function will separate latters in capital & small
+function smallCapitalLatter(arr) {
+    let capiatlLatter = [];
+    let smallLatter = [];
+    for (let i=0 ; i<arr.length ; i++){
+        if (arr[i] == arr[i].toUpperCase()){
+            capiatlLatter.push(arr[i]);
+        }
+        else {
+            smallLatter.push(arr[i]);
+        }
+    }
+    console.log(capiatlLatter);
+    console.log(smallLatter);
+}
+
+smallCapitalLatter(latter)
