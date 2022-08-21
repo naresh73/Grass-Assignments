5b02b310279f228d120ab0a53c76a59aaf55acbb
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-08-21 18:07:36 +0530

    separate even odd from an array

---
+++ day-5-DSA/evenodd.js
@@ -0,0 +1,19 @@
+const digits = [23,34,56,78,90,1,12,234];
+
+//this function will separate an Array in even and odd
+function EvenOdd(arr) {
+    let even = [];
+    let odd = [];
+    for (let i=0 ; i<arr.length ; i++) {
+        if (arr[i] % 2 == 0) {
+            even.push(arr[i]);
+        }
+        else {
+            odd.push(arr[i]);
+        }
+    }
+    console.log("Even digits = ",even);
+    console.log("Odd digits = ",odd);
+}
+
+EvenOdd(digits);
