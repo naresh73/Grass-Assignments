d5b426eb1938e29656903b060c4790d8d1fee323
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-08-26 07:24:50 +0530

    CRUD

--- day-11-CRUD/server.js
+++ day-11-CRUD/server.js
@@ -4,18 +4,54 @@ const connectToDB = require("./db");
 const PORT = 8000;
 const SportModel = require('./model/sports.model');
 
+//connecting Database
 connectToDB();
 
-myDBapp.use(express.json);
-myDBapp.listen(PORT, console.log('Server is listining on port ' + PORT));
+//allowing json objects
+myDBapp.use(express.json());
 
 //creating routes
-myDBapp.post('/sport', async (req, res () {
-    const sportData = new SportModel(req.body);
+myDBapp.post("/sport", async function (req, res) {
+    const sportNewModel = new SportModel(req.body);
     try {
-        await sportData.save();
-        res.send('Sports data saved', sportData)
+        await sportNewModel.save();
+        res.send("Sports data saved");
     } catch (error) {
-        
+        res.send(error.message);
     }
-}) )
+    // console.log(req.body);
+});
+
+myDBapp.get('/sport', async function(req, res) {
+    const sportss = await SportModel.find({});
+    // res.send("hey team");
+
+    try {
+        res.send(sportss);
+      } catch (error) {
+        res.send(error);
+      }
+});
+
+// updating routes
+myDBapp.put("/sport/:id", async function (req, res) {
+    try {
+      await SportModel.findByIdAndUpdate(req.params.id, req.body);
+      res.send("Data updated->>>>>>>>>>>>>>>");
+    } catch (error) {
+      res.send(error.message);
+    }
+  });
+  
+  // deleting routes
+  myDBapp.delete("/sport/:id", async function (req, res) {
+    try {
+      // findByIdAndDelete takes 1 argument called id
+      const sport = await SportModel.findByIdAndDelete(req.params.id);
+      res.send("Data deleted successfully->>>>>>>>>>>>>");
+    } catch (error) {
+      res.send(error.message);
+    }
+  });
+
+myDBapp.listen(PORT, console.log('Server is listining on port ' + PORT));
