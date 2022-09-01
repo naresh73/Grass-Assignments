1acf784e7623682ea7f9b5b424e53ee5e90acc7b
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-09-01 10:42:52 +0530

    App name changed into app

--- day-13-CRUD-assigments/controllers/food.controler.js
+++ day-13-CRUD-assigments/controllers/food.controler.js
@@ -1,46 +1,46 @@
 const FoodModel = require('../models/food/food.model');
 
-async function foodGet(request, result) {
+async function foodGet(request, response) {
     const CollectionOfFoods = await FoodModel.find({});
     try {
-        result.send(CollectionOfFoods);
+        response.send(CollectionOfFoods);
     } catch (error) {
-        result.send({message:error.message});
+        response.send({message:error.message});
     }
 }
 
-async function foodPost(request, result) {
+async function foodPost(request, response) {
     if(!request.body.Food_Name && !request.body.calories){
-        result.send({message: "cotents can not be empty"});
+        response.send({message: "cotents can not be empty"});
     }
 
     const NewFood = new FoodModel(request.body);
     try {
         await NewFood.save();
-        result.send({
+        response.send({
             message: "Food Added successfully",
             food_description: NewFood
         });
     } catch (error) {
-        result.send({message:error.message});
+        response.send({message:error.message});
     }
 }
 
-async function foodPut(request, result) {
+async function foodPut(request, response) {
     try {
         await FoodModel.findByIdAndUpdate(request.params.id, request.body);
-        result.send("Food data updated successfully->>>>>>>>>>>>>>>>>>")
+        response.send("Food data updated successfully->>>>>>>>>>>>>>>>>>")
     } catch (error) {
-        result.send({message:error.message});
+        response.send({message:error.message});
     }
 }
 
-async function foodDelete(request, result) {
+async function foodDelete(request, response) {
     try {
         await FoodModel.findByIdAndDelete(request.params.id);
-        result.send("Food data deleted successfully->>>>>>>>>>>>>>>>>>")
+        response.send("Food data deleted successfully->>>>>>>>>>>>>>>>>>")
     } catch (error) {
-        result.send({message:error.message});
+        response.send({message:error.message});
     }
 }
 
